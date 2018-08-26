2bf29083beaa6530a7b28900a9e34759324d133a
gaggle331 <hafezg@email.arizona.edu>
2018-08-26 12:04:29 -0700

    Adding logic for playing potions.

--- src/main/java/actions/PlayCardFromHandAction.java
+++ src/main/java/actions/PlayCardFromHandAction.java
@@ -6,6 +6,8 @@ import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.potions.AbstractPotion;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
 import jsonUtil.JsonDump;
 
 
@@ -24,12 +26,27 @@ public class PlayCardFromHandAction extends com.megacrit.cardcrawl.actions.Abstr
     	JsonDump jsonDump = new JsonDump();
     	jsonDump.createJson();
 
+
 		if (AbstractDungeon.player.hand.group.size() > 0) { //something to do with hand being empty after the last card is played????
 			int n = 0;
 			while (n<AbstractDungeon.getCurrRoom().monsters.monsters.size()-1 && (AbstractDungeon.getCurrRoom().monsters.monsters.get(n).isDead || AbstractDungeon.getCurrRoom().monsters.monsters.get(n).isDying)) {
 				n = n + 1;
 			}
 			AbstractMonster m = AbstractDungeon.getCurrRoom().monsters.monsters.get(n);
+			if (!p.potions.isEmpty())
+			{
+				for (AbstractPotion potion : p.potions)
+				{
+					potion.use(m);
+					for (final AbstractRelic r : AbstractDungeon.player.relics) {
+						r.onUsePotion();
+					}
+					System.out.println("potion in slot: " + potion.slot);
+					if (!"Potion Slot".equalsIgnoreCase(potion.name) ) {
+						AbstractDungeon.topPanel.destroyPotion(potion.slot);
+					}
+				}
+			}
 			int q = 0;
 			while (q < p.hand.group.size()-1 && !AbstractDungeon.player.hand.group.get(q).canUse(p, m)) {
 				q = q + 1;
